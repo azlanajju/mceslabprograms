9e368d4b8150a1404a4a6716b486a65567a3cc74
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-08-21 03:10:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
